7a7f5c447c80b691a0a60ae7788dd0eabb432cde
ipetrovanton <ipetrovanton@gmail.com>
2020-03-02 20:18:56 +0300

    Add DocxToXnl

---
+++ ConverterToXml.Test/DocxToXmltest.cs
@@ -0,0 +1,26 @@
+﻿using ConverterToXml.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace ConverterToXml.Test
+{
+    public class DocxToXmltest
+    {
+        [Fact]
+        public void XlsxConverterTest_NotNull()
+        {
+            DocxToXml converter = new DocxToXml();
+            string curDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string path = curDir + @"/Files/docx.docx";
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                string result = converter.Convert(fs);
+                Assert.NotNull(result);
+            }
+        }
+    }
+}
